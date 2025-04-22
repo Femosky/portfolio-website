ade75d981e507d5f48b4f953172bf5d6baf68f7d
Olufemi Ojeyemi <olufemiojeyemi@gmail.com>
2025-04-21 20:44:13 -0400

    ShowcaseItem image size fix

--- data/showcaseData.js
+++ data/showcaseData.js
@@ -42,7 +42,7 @@ function RokiShowcaseTitle() {
     return (
         <h3 className="flex flex-col md:flex-row items-center justify-center gap-1 text-lg md:text-xl text-center">
             Download my first ever published app,
-            <span className="flex items-center gap-[1px] italic">
+            <span className="flex items-center gap-[1px] italic w-5">
                 <img className="w-5" src="/roki-logo.png" alt="roki app logo" />
                 oki
             </span>
